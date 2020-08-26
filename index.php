cca13b1ba87afcfea88c1af27e3347eaa109874b
caooss <rafael.r.l.pavan@gmail.com>
2020-08-26 18:34:06 -0300

    Inserindo uma página inicial em index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title>Sequência de Fibonacci</title>
+    </head>
+    <body>
+        <form action="fibonacci.php" method="post">
+            <fieldset>
+                <legend>Sequencia De Fibonacci</legend>
+                <p>Qual o Tamnho que deseja em sua sequência: <input type="number" name="sequencia"/></p>
+
+                <input type="submit" value="Enviar"/>
+            </fieldset>
+        </form>
+    </body>
+</html>
